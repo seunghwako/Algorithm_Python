0a5c879bbe156bca1084935a6979497bfc67db38
seunghwako <seunghwa2475@naver.com>
2020-12-18 11:22:17 +0900

    boj_11021

---
+++ for문/boj11021.py
@@ -0,0 +1,12 @@
+n = input()
+n = int(n)
+result = []
+
+
+for i in range(1, n+1) : 
+    a,b = map(int, input().split())
+    if a > 0  and a < 10 and b > 0 and b < 10 :
+        result.append("Case #" + str(i) +  ": " + str(a+b))
+
+for r in result :
+    print(r)
